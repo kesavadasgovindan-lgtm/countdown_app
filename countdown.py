33ac0113eadfd2e8446a301fc33222ea4ae70ee4
Kesavadas Govindan <kesavadasgovindan@gmail.com>
2026-09-25 19:55:41 +0530

    Implemented Rowfactory

--- countdown.py
+++ countdown.py
@@ -40,6 +40,7 @@ def check_table_exists(table_name):
 @app.command("list")
 def list_events(private: Annotated[bool, typer.Option("--private")] = False):
     with sqlite3.connect(DB_PATH) as conn:
+        conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
         query = "SELECT id, name, date, priority, is_private FROM events"
         
@@ -58,17 +59,21 @@ def list_events(private: Annotated[bool, typer.Option("--private")] = False):
         table.add_column("Days Left", style="blue")
 
         for event in events:
-            event_id, name, date_str, priority, is_private = event
-            date = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
-            days_left = (date - datetime.date.today()).days
+            event_date = datetime.datetime.strptime(event["date"], "%Y-%m-%d").date()
+            days_left = (event_date - datetime.date.today()).days
+            if days_left < 0:
+               msg = f"[red]{abs(days_left)} days ago[/red]"
+            elif days_left == 0:
+                msg = "[yellow]Today[/yellow]"
+            else:
+                msg = f"[green]{days_left} days left[/green]"
             table.add_row(
-                str(event_id),
-                name,
-                date_str,
-                "Yes" if priority else "No",
-                "Yes" if is_private else "No",
-                str(days_left),
-            )   
+                str(event["id"]),
+                event["name"],
+                event["date"],
+                "Yes" if event["priority"] else "No",
+                "Yes" if event["is_private"] else "No",
+                msg,)
 
         console.print(table)
 
